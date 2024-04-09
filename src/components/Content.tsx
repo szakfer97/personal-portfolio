6bb89a77bcd42ec1ea0f601c177dfb81c87e55a6
szakferi <szakfer97@gmail.com>
2024-04-09 18:49:02 +0300

    Added base Footer

---
+++ src/components/Content.tsx
@@ -0,0 +1,7 @@
+export default function Content() {
+  return (
+    <div>
+      <p className="text-3xl font-bold underline">Hello world!</p>
+    </div>
+  );
+}
---
+++ src/components/Footer.tsx
@@ -0,0 +1,47 @@
+export default function Footer() {
+  return (
+    <footer className="bg-gray-800 text-gray-200 text-center py-4">
+      <div className="container mx-auto px-4">
+        <p className="text-sm">
+          Made with &#128150; by{" "}
+          <a
+            href="https://www.linkedin.com/in/szakacsi-ferenc-adam/"
+            target="_blank"
+            rel="noreferrer"
+            className="text-blue-600 hover:underline"
+          >
+            Szakacsi Ferenc-Adam
+          </a>
+        </p>
+        <p className="text-xs">
+          Built with{" "}
+          <a
+            href="https://vitejs.dev/"
+            target="_blank"
+            rel="noreferrer"
+            className="text-blue-600 hover:underline"
+          >
+            Vite,
+          </a>{" "}
+          <a
+            href="https://reactjs.org/"
+            target="_blank"
+            rel="noreferrer"
+            className="text-blue-600 hover:underline"
+          >
+            React
+          </a>{" "}
+          and
+          <a
+            href="https://www.typescriptlang.org/"
+            target="_blank"
+            rel="noreferrer"
+            className="text-blue-600 hover:underline"
+          >
+            TypeScript
+          </a>{" "}
+        </p>
+      </div>
+    </footer>
+  );
+}
