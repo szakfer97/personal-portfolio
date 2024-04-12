db650d920836804f09a8909c955dc3c60cce4586
szakferi <szakfer97@gmail.com>
2024-04-12 23:02:21 +0300

    Further content to Footer component

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,54 +1,15 @@
 export default function Footer() {
+  const currentYear = new Date().getFullYear();
   return (
     <footer className="bg-gray-800 text-gray-200 text-center py-4">
       <div className="container mx-auto px-4">
-        <p className="text-sm">
-          Made with &#128150; by{" "}
-          <a
-            href="https://www.linkedin.com/in/szakacsi-ferenc-adam/"
-            target="_blank"
-            rel="noreferrer"
-            className="text-blue-600 hover:underline"
-          >
-            Szakacsi Ferenc-Adam
-          </a>
-        </p>
-        <p className="text-xs">
-          Built with{" "}
-          <a
-            href="https://vitejs.dev/"
-            target="_blank"
-            rel="noreferrer"
-            className="text-blue-600 hover:underline"
-          >
-            Vite,
-          </a>{" "}
-          <a
-            href="https://reactjs.org/"
-            target="_blank"
-            rel="noreferrer"
-            className="text-blue-600 hover:underline"
-          >
-            React
-          </a>{" "}
-          and{" "}
-          <a
-            href="https://www.typescriptlang.org/"
-            target="_blank"
-            rel="noreferrer"
-            className="text-blue-600 hover:underline"
-          >
-            TypeScript
-          </a>{" "}
-          using{" "}
-          <a
-            href="https://tailwindcss.com/"
-            target="_blank"
-            rel="noreferrer"
-            className="text-blue-600 hover:underline"
-          >
-            Tailwind CSS
-          </a>
+        <p className="text-center text-xs text-gray-400 sm:text-sm">
+          <span className="block">
+            Copyright &copy; {currentYear} All rights reserved.
+          </span>
+          <span className="sm:ml-auto inline-block sm:mt-0 mt-2 align-baseline">
+            Made by Szakacsi Ferenc-Adam
+          </span>
         </p>
       </div>
     </footer>
