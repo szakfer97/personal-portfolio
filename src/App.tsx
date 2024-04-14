2db49260a0739d8adb552783274f096e57b0668c
szakferi <szakfer97@gmail.com>
2024-04-14 16:45:34 +0300

    Added content to Skills component

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import Contact from "./components/Contact";
 import Footer from "./components/Footer";
 import NavBar from "./components/NavBar";
+import Skills from "./components/Skills";
 
 export default function App() {
   return (
     <div>
       <NavBar />
       <Contact />
+      <Skills />
       <Footer />
     </div>
   );
--- src/components/Skills.tsx
+++ src/components/Skills.tsx
@@ -1,3 +1,95 @@
+import {
+  FaHtml5,
+  FaCss3,
+  FaJs,
+  FaReact,
+  FaNodeJs,
+  FaGithub,
+  FaGitlab,
+  FaJira,
+} from "react-icons/fa";
+import { SiTypescript, SiMongodb, SiExpress, SiJest } from "react-icons/si";
+
 export default function Skills() {
-  return <div></div>;
+  const icons = [
+    {
+      Icon: FaJs,
+      name: "JavaScript",
+      url: "https://www.ecma-international.org/",
+    },
+    {
+      Icon: FaHtml5,
+      name: "HTML",
+      url: "https://html.spec.whatwg.org/",
+    },
+    {
+      Icon: FaCss3,
+      name: "CSS",
+      url: "https://www.w3.org/Style/CSS/",
+    },
+    {
+      Icon: FaReact,
+      name: "React",
+      url: "https://reactjs.org/",
+    },
+    {
+      Icon: SiTypescript,
+      name: "TypeScript",
+      url: "https://www.typescriptlang.org/",
+    },
+    {
+      Icon: FaNodeJs,
+      name: "Node.js",
+      url: "https://nodejs.org/",
+    },
+    {
+      Icon: SiExpress,
+      name: "Express",
+      url: "https://www.expressjs.com/",
+    },
+    {
+      Icon: SiMongodb,
+      name: "MongoDB",
+      url: "https://www.mongodb.com/",
+    },
+    {
+      Icon: SiJest,
+      name: "Jest",
+      url: "https://www.jestjs.com/",
+    },
+    {
+      Icon: FaJira,
+      name: "Jira",
+      url: "https://www.atlassian.com/software/jira",
+    },
+    {
+      Icon: FaGithub,
+      name: "GitHub",
+      url: "https://github.com/",
+    },
+    {
+      Icon: FaGitlab,
+      name: "GitLab",
+      url: "https://about.gitlab.com/",
+    },
+  ];
+
+  return (
+    <div className="grid gap-4 md:grid-cols-3 grid-cols-1 place-items-center">
+      {icons.map(({ Icon, name, url }, i) => (
+        <a
+          href={url}
+          target="_blank"
+          rel="noopener noreferrer"
+          className="self-center"
+          key={i}
+        >
+          <Icon size={80} className="mb-4" />
+          <p className="text-center text-sm mt-2 mb-4 italic underline-offset-8 underline uppercase">
+            {name}
+          </p>
+        </a>
+      ))}
+    </div>
+  );
 }
